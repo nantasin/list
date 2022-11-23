1a3efaa65706c83f64ba40f54b2de065a6684218
nantasin <116702776+nantasin@users.noreply.github.com>
2022-11-23 12:12:42 +0700

    Add files via upload

---
+++ list_element.py
@@ -0,0 +1,8 @@
+goals = ['call to granma','shopping','cook dinner']
+print(goals)
+i = input('which goals do you want to chage? ')
+goals[i] = input("anter a new goals: ")
+print(goals)
+#i = input('which goals do want to print?: ')
+#i = int(i) # convert litter(str) tothe number (intoger)
+#print(goals(i))
