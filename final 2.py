dcafd30e3375eabfaa34d2b9acc604b988d9d299
nantasin <116702776+nantasin@users.noreply.github.com>
2023-01-25 12:10:47 +0700

    Add files via upload

---
+++ final 2.py
@@ -0,0 +1,12 @@
+cities = ["Ratchburi","Nan","Chanthaburi","Lay","Kampgaeng Phet"]
+
+ for i in range(ian(cities)):
+    print(i, cities[i])
+
+new_city = input("Enter a new city to add to the list: ")
+cities.append(new_city)
+
+delete_city = input("Enter the name of a city todelete from the list: ")
+del cities{delete}
+
+print(cities)
